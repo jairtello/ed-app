a9a38fe7087547dcca4b79e220b5569e0c0cbd9d
Jair Tello <jairdeyner730791@gmail.com>
2020-11-22 16:16:46 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Curso from "./Curso";
+import "./styles/styles.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        className="main-banner img-container diagonal l-section"
+        id="main-banner"
+      >
+        <div className="ed-grid lg-grid-6">
+          <div className="lg-cols-4 lg-x-2">
+            <img
+              className="main-banner__img"
+              src="https://images.pexels.com/photos/265614/pexels-photo-265614.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
+              alt="Bckground Banner"
+            />
+            <div className="main-banner__data s-center">
+              <p className="t2 s-mb-0">TVJair</p>
+              <p>Comienza tu futuro desde hoy</p>
+              <a href="#hola" className="button">
+                Tomar clases
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="ed-grid m-grid-3">
+        <Curso /> <Curso /> <Curso /> <Curso /> <Curso />
+        <Curso /> <Curso /> <Curso /> <Curso />
+      </div>
+    </>
   );
 }
 
